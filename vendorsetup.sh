7c9028f1a45048036bfa0cceb0e795629d1e2b95
Eyad Tamer <eyadtamer2121@gmail.com>
2026-05-27 20:27:03 +0300

    Create vendorsetup.sh
    The main goal of this is that it lets us disable the flashlight feature in OrangeFox because our device is a tablet which doesn't even have a flashlight.

---
+++ vendorsetup.sh
@@ -0,0 +1,53 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2026 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="X505X"
+
+fox_get_target_device() {
+  if echo "$BASH_SOURCE" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE";
+  elif set | grep BASH_ARGV | grep -w \"$FDEVICE\"; then
+      FOX_BUILD_DEVICE="$FDEVICE";
+  elif echo "${BASH_SOURCE[0]}" | grep -q "/$FDEVICE/"; then
+      FOX_BUILD_DEVICE="$FDEVICE";
+  elif echo "$0" | grep -q "$FDEVICE"; then
+      FOX_BUILD_DEVICE="$FDEVICE";
+  fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export LC_ALL="C"
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+
+	export TARGET_DEVICE_ALT="TB-X505X"
+
+  export OF_FLASHLIGHT_ENABLE=0
+
+  export OF_DEFAULT_KEYMASTER_VERSION=4.0
+
+	export FOX_VANILLA_BUILD=1
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
+fi
