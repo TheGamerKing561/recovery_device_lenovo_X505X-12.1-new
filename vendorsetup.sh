ca0a67038025fd9e12c62a25dd0074cdee100e8b
Eyad Tamer <eyadtamer2121@gmail.com>
2026-06-21 01:14:27 +0300

    Try OF_SCREEN_H as 1280

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -48,7 +48,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
     export OF_ENABLE_FRP_ADDON=1
 
-	export OF_SCREEN_H=1200
+	export OF_SCREEN_H=1280
   
     # Check whether we are building OrangeFox or not
     if [ -f "$(gettop)/bootable/recovery/orangefox.mk" ]; then
